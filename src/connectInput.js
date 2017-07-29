28fc64cb4918737f607fdb78384eb17ff98f89ab
Kai Curry <kai@cape.io>
2017-07-29 01:04:04 -0400

    connectInput

---
+++ src/connectInput.js
@@ -0,0 +1,13 @@
+import { bindActionCreators } from 'redux'
+import { createStructuredSelector } from 'reselect'
+import { connect } from 'react-redux'
+import { getFormEvents, selectFieldValue, getPrefix } from 'redux-field'
+
+export const mapStateToProps = createStructuredSelector({
+  value: selectFieldValue,
+})
+
+export const mapDispatchToProps = (dispatch, ownProps) =>
+  bindActionCreators(getFormEvents(getPrefix(ownProps)), dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)
